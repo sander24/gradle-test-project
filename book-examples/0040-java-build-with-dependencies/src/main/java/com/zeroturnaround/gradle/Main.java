4ea9407f036d9a124e42f2eda4e9b47cec69e964
sander <sander.sonajalg@gmail.com>
2013-06-27 17:24:33 +0300

    build with dependencies

---
+++ book-examples/0040-java-build-with-dependencies/src/main/java/com/zeroturnaround/gradle/Main.java
@@ -0,0 +1,21 @@
+package com.zeroturnaround.gradle;
+
+import com.zeroturnaround.gradle.Greet;
+
+public class Main {
+
+  public static void main(String[] args) {
+    
+    // try to get the name from the arguments
+    String name = null;
+    if (args.length > 0) {
+      name = args[0];
+    }
+    
+    Greet greet = new Greet(name);
+    
+    //log.debug(greet.greet());
+    System.out.println(greet.greet());
+  }
+
+}
