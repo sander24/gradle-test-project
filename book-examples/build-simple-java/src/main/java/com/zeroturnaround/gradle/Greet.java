cba2b9ca4cb80220cbdcd344a7d2e0204d53c278
sander <sander.sonajalg@gmail.com>
2013-06-27 14:16:53 +0300

    sample: build simple java code

---
+++ book-examples/build-simple-java/src/main/java/com/zeroturnaround/gradle/Greet.java
@@ -0,0 +1,11 @@
+package com.zeroturnaround.gradle;
+
+public class Greet {
+
+  public Greet() {}
+  
+  public String greet() {
+    return "Hello!";
+  }
+
+}
---
+++ book-examples/build-simple-java/src/main/java/com/zeroturnaround/gradle/Main.java
@@ -0,0 +1,10 @@
+package com.zeroturnaround.gradle;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Greet greet = new Greet();
+    System.out.println(greet.greet());
+  }
+
+}
